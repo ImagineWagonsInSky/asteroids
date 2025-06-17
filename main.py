ed512cdbe717a912829e891e0e1b4ae46577ccc2
ImagineWagonsInSky <will18.perfect@gmail.com>
2025-06-17 19:27:29 +0200

    Player Sprite and Groups added

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             # window close button
             if event.type == pygame.QUIT:
                 return
+            
+        updatables.update(dt)
         screen.fill("black")
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
     
 
 
